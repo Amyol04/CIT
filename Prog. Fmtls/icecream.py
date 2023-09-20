d197523bcfab8a5eb49bc1e02f2cac108d6b5ea4
Amy O Leary! <amyoleary404@gmail.com>
2023-09-20 16:26:32 +0100

    Create icecream.py

---
+++ Prog. Fmtls/icecream.py
@@ -0,0 +1,15 @@
+# author : Amy O leary
+# desciption : Icecream and prices go brr  
+
+icecream = "Vanilla"
+
+price = 2.35
+
+scoops = 3
+
+print("The icecream is ", type(icecream))
+print("The Price is ", type(price))
+print("The number of scoops is ", type(scoops))
+
+total = price * scoops
+print(f" The Total is: {total} :)")
