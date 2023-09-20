2fc2126a4f741c3ee9c1cb09e450837cb85ec21f
Amy O Leary! <amyoleary404@gmail.com>
2023-09-20 16:27:04 +0100

    Create LondonBridge.py

---
+++ Prog. Fmtls/LondonBridge.py
@@ -0,0 +1,17 @@
+# Author: Amy o leary
+# Description: London Bridge Nursery Rhyme 
+
+bridge = "London Bridge is"
+falling = "falling down"
+lady = "My fair lady"
+
+print(bridge, falling, falling, falling)
+print(bridge, falling)
+print(lady)
+
+build = ("Build it up with wood and Clay")
+wood = ("wood and clay")
+
+print(build,wood, wood, )
+print(build)
+print(lady)
