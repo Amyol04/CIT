36d8c7836811d2ed79806177eb3b04a460992de0
Amy O Leary! <amyoleary404@gmail.com>
2023-09-27 16:22:47 +0100

    Create triangle.py

---
+++ Week2/triangle.py
@@ -0,0 +1,6 @@
+base = float(input("What is the base of the triangle?"))
+height = float(input("What is the height of the triangle?"))
+
+area = base * height
+
+print(f"the area of this Trianle is {area}")
