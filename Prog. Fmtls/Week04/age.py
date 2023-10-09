3301eca2ebec75b091f67516d541e9dc6706e089
Amy O Leary! <amyoleary404@gmail.com>
2023-10-09 09:47:03 +0100

    Create age.py

---
+++ Prog. Fmtls/Week04/age.py
@@ -0,0 +1,11 @@
+myAge = 19 
+
+age = int(input("What age are you"))
+
+if age == myAge: 
+    print("We are the same age")
+
+elif age < myAge: 
+    print("You are younger then i am")
+else: 
+    print("You are older then i am")
