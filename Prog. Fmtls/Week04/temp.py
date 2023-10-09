e276e6b0147e2ad7d7c2e60d143bc5ab68b06d5c
Amy O Leary! <amyoleary404@gmail.com>
2023-10-09 09:41:26 +0100

    Create temp.py

---
+++ Prog. Fmtls/Week04/temp.py
@@ -0,0 +1,11 @@
+temp = float(input("What is the temp? "))
+hot = 32 
+cold = 4
+
+if temp > hot :
+    print("it is v hot")
+
+elif temp < cold :
+    print("it is cold")
+else:
+    print("good weather :)")
