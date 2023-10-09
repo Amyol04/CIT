7cec182e357a535b1f01e9feb199b8f47f464f09
Amy O Leary! <amyoleary404@gmail.com>
2023-10-09 12:07:15 +0100

    Create population.py

---
+++ Prog. Fmtls/Week03 /population.py
@@ -0,0 +1,17 @@
+pop_of_china = 1409
+pop_of_india = 1339
+pop_of_america = 324
+pop_of_ireland = 4
+
+county1 = "china" 
+county2 = "india"
+county3 = "america"
+county4 = "ireland"
+
+
+print(f"{'county':10}{'population!':>10}")
+print("-----------------------")
+print(f"{county1}<10{pop_of_china:<10,}")
+print(f"{county2}<10{pop_of_india:<10,}")
+print(f"{county3}<10{pop_of_america:<10,}")
+print(f"{county4}<10{pop_of_ireland:<10,}")
