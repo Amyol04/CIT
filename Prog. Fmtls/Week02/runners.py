fee15e103ed2bf6a8c4a8a42b8a4e6d1398e5bb2
Amy O Leary! <amyoleary404@gmail.com>
2023-10-09 12:06:22 +0100

    Create runners.py

---
+++ Prog. Fmtls/Week02/runners.py
@@ -0,0 +1,11 @@
+name1 = input("what is ur name dawg")
+name2 = input("what is ur name dawg")
+
+time1 = int(input("how much time did it take "))
+time2 = int(input("how much time did it take "))
+
+time = time1 // 60  
+remaining_hours = time1 % 60
+
+time3 = time2 // 60  
+remaining_hours = time2 % 60
