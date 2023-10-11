e278f01a63e221b592ec1b29eacc10b826a4eab5
Amy O Leary! <amyoleary404@gmail.com>
2023-10-11 16:22:07 +0100

    Create numberSeason.py

---
+++ Prog. Fmtls/Week04/numberSeason.py
@@ -0,0 +1,13 @@
+number = int(input("gimme a number "))
+
+
+if number == 1: 
+    print("This number repersents Winter")
+elif number == 2: 
+    print("this number represents Spring")  
+elif number == 3: 
+    print("this number represents Summer")
+elif number == 4: 
+    print("this number represents Autumn")
+else: 
+    print(" try again , pick a diff number ")
