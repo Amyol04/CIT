862ef91e5a1d4652bb1347e12d297740ab29acc8
Amy O Leary! <amyoleary404@gmail.com>
2023-10-23 09:17:01 +0100

    Update Lect.py

--- Prog. Fmtls/Week06/Lect.py
+++ Prog. Fmtls/Week06/Lect.py
@@ -13,3 +13,16 @@ Vowels = 'A','E','I','O','U'
 
 if letter in Vowels:
     print("Vowel")
+    
+    
+######## String
+
+Import string
+s = "The rain will never stop"
+
+print(string.ascii_lowercase) # puts into lower case
+print(string.punctuation)
+
+letter = x 
+if letter in string.ascii_lowercase: 
+    print("this is lower case")
