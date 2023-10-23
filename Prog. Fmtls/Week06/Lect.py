80111a17360fd8bea98ad08636ac1587187ac982
Amy O Leary! <amyoleary404@gmail.com>
2023-10-23 09:12:10 +0100

    Update Lect.py

--- Prog. Fmtls/Week06/Lect.py
+++ Prog. Fmtls/Week06/Lect.py
@@ -1,1 +1,15 @@
-hshdh
+s = "the rain will never stop"
+
+#in operator - is lhs item in lhs item
+
+if "o" in s 
+    print("there is an o in the sentence")
+
+letter = x 
+if letter in 'aeiouAEIOU':
+    print("Vowel")
+    
+Vowels = 'A','E','I','O','U'
+
+if letter in Vowels:
+    print("Vowel")
