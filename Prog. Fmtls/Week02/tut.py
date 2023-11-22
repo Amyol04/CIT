e58080f67065f82a45d010c6736c7b345c86d448
Amy O Leary! <amyoleary404@gmail.com>
2023-11-22 09:54:46 +0000

    Update tut.py

--- Prog. Fmtls/Week02/tut.py
+++ Prog. Fmtls/Week02/tut.py
@@ -1,119 +1,83 @@
 #ProgFundem #2
-print("\U0001F973", "\U0001F973", sep='******') #\u0001F73 - Unicode for emojis
+#                                   ∞༺✦✮✦༻✧ Emoji and Arithmetic Operations ∞༺✦✮✦༻✧
+print("\U0001F973", "\U0001F973", sep='******')  # Unicode for emojis
 i = 7
 f = 6.7
-7 // 5 # float division
-7 %  5# division with a remainder
-7 ** 5 #7 to the power of 5 # can be decimal (learned in lab)
-import math #lab recap #adds math functions (cos, pi, ect...) (inf-infinate nan-not a number)
-math.pi #example
-# Recap ^^^
+result1 = 7 // 5                      # Float division
+result2 = 7 % 5                       # Division with remainder
+result3 = 7 ** 5                      # 7 to the power of 5
 
 
+#                                   ∞༺✦✮✦༻✧   Importing Math Module ∞༺✦✮✦༻✧ 
+import math  # Adds math functions (cos, pi, etc...)
+pi_value = math.pi  # Example using math.pi
+
+#                                  ∞༺✦✮✦༻✧   User Input and Data Types ∞༺✦✮✦༻✧
 class_name = input("What is the Class' name? ")
-print(type(class_name), class_name) #(input function always a string)
+print(type(class_name), class_name)  # input function always returns a string
 
-
+#                            ∞༺✦✮✦༻✧  String Concatenation and Type Conversion ∞༺✦✮✦༻✧
 num1 = input("Number 1 >> ")
 num2 = input("Number 2 >> ")
-add = num1 + num2
-print(type(add), add) #(adds as strings ~~ 7 + 9.6 = 79.6) (cant do minus between 2 strings)
+add_as_string = num1 + num2  # Adds as strings (e.g., "7" + "9.6" = "79.6")
 
+#                                  ∞༺✦✮✦༻✧ Type Conversion to Integers ∞༺✦✮✦༻✧
 
-num1 = int(input("Number 1 >> ")) #add int(interager) to convert string to int
+num1 = int(input("Number 1 >> "))  # Convert string to int
 num2 = int(input("Number 2 >> "))
-add = num1 + num2
-print(type(add), add)
+add_as_int = num1 + num2  # Adds as integers
 
+#                                    ∞༺✦✮✦༻✧ Type Conversion to Floats  ∞༺✦✮✦༻✧
 
-num1 = float(input("Number 1 >> ")) #add float to convert string to float
+num1 = float(input("Number 1 >> "))  # Convert string to float
 num2 = float(input("Number 2 >> "))
-add = num1 + num2
-print(type(add), add)
+add_as_float = num1 + num2  # Adds as floats
 
-
-
-
-# functions- types ^^^
-
-
-#int - no decimal
-#float - decimal number
-
-
-#exersise  
-#1
+#                                   ♡━━━♡ Exercise 1: Coffee Cost Calculation ♡━━━♡
 coffee_price = 2.20
 quantity = int(input("How many coffees would you like?"))
-total_cost = coffee_pice * quantity
-print(f"Total Cost for {quantity} cups of coffee is: "f"€{total_cost:.2f}") # :.2f  colon: instructions .2f- 2 digits after the decimal
+total_cost = coffee_price * quantity
+print(f"Total Cost for {quantity} cups of coffee is: €{total_cost:.2f}")
 
-
-#2
-description = (input("Description of Product"))
+#                                  ♡━━━♡ Exercise 2: Product Cost Calculation with Discount ♡━━━♡
+description = input("Description of Product")
 cost = float(input("How Much is The Product"))
 reduction = float(input("Discount?"))
-total_cost = cost - price*(1 - reduction)
-print(f"The Total cost for {description.lower} is: "f"€{total_cost:.2f}"f"with the student discount.")
+total_cost_with_discount = cost - cost * (1 - reduction)
+print(f"The Total cost for {description.lower()} is: €{total_cost_with_discount:.2f} with the student discount.")
 
-
-#3
+#                            ♡━━━♡ Exercise 3: Circle Area and Circumference Calculation ♡━━━♡
 import math
-radius = float(input("What is the circles radius?"))
-
-
+radius = float(input("What is the circle's radius?"))
 area = math.pi * radius ** 2
-circ = 2 * math.pi * radius
-print(f"Area is {area:.4f} and circumerence is {circ:.4f}")
+circumference = 2 * math.pi * radius
+print(f"Area is {area:.4f} and circumference is {circumference:.4f}")
 
-
-
-
-name1 = "conan"
-name2 = "amy"
-name3 = "james"
-
-
-age1 = 5
-age2 = 19
-age3 = 19
-
-
-name_heading = "name"
-age_heading = "age"
-
-
-print(f"{name_heading:^12}{age_heading:^10}") #< -left > - right ^- middle
-# print(f"{'Name':^12}{'Age':^10}") #alternative way with minimal variables
-print('-') *22
-print(f"{name1:>12}{age1:10}") # :10 - colum of 10/12
-print(f"{name2:>12}{age2:10}")#  :> override defualt behaviour - letters left & numbers right
+#                                ∞༺✦✮✦༻✧   F-Strings for Formatting ∞༺✦✮✦༻✧
+name1, name2, name3 = "Conan", "Amy", "James"
+age1, age2, age3 = 5, 19, 19
+name_heading, age_heading = "Name", "Age"
+print(f"{name_heading:^12}{age_heading:^10}")  # Center align headings
+print('-' * 22)
+print(f"{name1:>12}{age1:10}")  # Right align names, left align ages
+print(f"{name2:>12}{age2:10}")
 print(f"{name3:>12}{age3:10}")
 
-
-#f strings - illustrating ^^^^
-
-
-
-
+#                             ∞༺✦✮✦༻✧ Formatting Percentages and Commas ∞༺✦✮✦༻✧
 discount = 0.2
-print(f"The Discount is {discount:%}.") # :.0%- Adds pecent sign afer colon ~ .0 - removes 6 digits after decimal
+print(f"The Discount is {discount:%}")  # Displays percentage with 0 decimal places
 wages = 12345.98
-print(f"The Wages are €{wages:,}.") #Adds comma every thousand/ where they need to be
-#Commas & percentages ^^^
+print(f"The Wages are €{wages:,.}")  # Adds commas for thousands in numbers
 
-
-#question
-title1 = input("title of the book : ")
+#                              ∞༺✦✮✦༻✧  Book Titles and Total Price Calculation ∞༺✦✮✦༻✧
+title1 = input("Title of the book: ")
 cost1 = float(input(f"Price of '{title1}': "))
-
-
-title2 = input("title of the book : ")
+title2 = input("Title of the book: ")
 cost2 = float(input(f"Price of '{title2}': "))
-total = cost1 + cost2
+total_price = cost1 + cost2
 
-
+#                             Displaying Book Details and Total Price
 print(f"{title1:20}€{cost1:.2f}")
 print(f"{title2:20}€{cost2:.2f}")
-print("="*30)
-print(f"'{Total Price:':20}€{total:.2f}")
+print("=" * 30)
+print(f"Total Price: {'€':>15}{total_price:.2f}")  # Right aligns and formats the total price
