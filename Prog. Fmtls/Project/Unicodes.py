8a2170902ec2392aa527e0901b032643e883a133
Amy O Leary! <amyoleary404@gmail.com>
2023-11-29 16:45:44 +0000

    Update Unicodes.py

--- Prog. Fmtls/Project/Unicodes.py
+++ Prog. Fmtls/Project/Unicodes.py
@@ -5,3 +5,8 @@ print('\U0001F920 Good job!')
 print('\U0001F614 Not bad, keep trying!')
 
 print('\U0001F641 You can do better, practice more!')
+
+
+https://unicode.org/emoji/charts/full-emoji-list.html
+
+#(add 000 to replace the + in the code) 
